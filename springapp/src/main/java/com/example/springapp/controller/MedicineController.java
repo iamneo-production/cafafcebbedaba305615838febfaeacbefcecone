5a0f817b83820440092408a8cc842b02f84c6cf7
Iamneo <techadmin@iamneo.ai>
2023-09-30 08:06:05 +0000

    Backup Commit

--- springapp/src/main/java/com/example/springapp/controller/MedicineController.java
+++ springapp/src/main/java/com/example/springapp/controller/MedicineController.java
@@ -1,3 +1,35 @@
+package com.yourpackage.controller;
+
+import com.yourpackage.model.Medicine;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/medicines")
 public class MedicineController {
-    
+    private Map<Integer, Medicine> medicineDatabase = new HashMap<>();
+    private int nextMedicineId = 1;
+
+    @PostMapping
+    public boolean addMedicine(@RequestBody Medicine medicine) {
+        medicine.setMedicineId(nextMedicineId++);
+        medicineDatabase.put(medicine.getMedicineId(), medicine);
+        return true;
+    }
+
+    @PutMapping("/{medicineId}")
+    public Medicine updateMedicine(@PathVariable int medicineId, @RequestBody Medicine updatedMedicine) {
+        if (medicineDatabase.containsKey(medicineId)) {
+            Medicine existingMedicine = medicineDatabase.get(medicineId);
+            existingMedicine.setMedicineName(updatedMedicine.getMedicineName());
+            existingMedicine.setPrice(updatedMedicine.getPrice());
+            existingMedicine.setQuantity(updatedMedicine.getQuantity());
+            existingMedicine.setDescription(updatedMedicine.getDescription());
+            return existingMedicine;
+        } else {
+            return null; // Handle the case where the medicine with the given ID does not exist
+        }
+    }
 }
